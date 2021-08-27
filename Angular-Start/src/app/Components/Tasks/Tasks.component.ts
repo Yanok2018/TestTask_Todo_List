51de22794fb2cd4f7f69523e8f73688963497604
Yana <yana2003kon@gmail.com>
2021-08-27 22:20:30 +0300

    Create some componens + fix some bugs on material

---
+++ Angular-Start/src/app/Components/Tasks/Tasks.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+@Component({
+  selector: 'Tasks',
+  templateUrl: './Tasks.component.html',
+  styleUrls: ['./Tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- Angular-Start/src/app/app.module.ts
+++ Angular-Start/src/app/app.module.ts
@@ -4,10 +4,19 @@ import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './Components/Navbar/Navbar.component';
+import { TasksComponent } from './Components/Tasks/Tasks.component';
+
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
